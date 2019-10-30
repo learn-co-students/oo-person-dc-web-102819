633c60fe869dd7bdfc2be4c663b32b649ceac30d
Flatiron School <stvik2@gmail.com>
2019-10-30 13:57:49 -0400

    Stage 1

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,38 @@
 # your code goes here
+
+#create Person Class
+class Person
+
+#initialize person class with name (constant), 
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness_points = 8
+        @hygine_points = 8
+    end
+#bank account with inital amount of 25 (no max or min)
+#happiness points initial amount of 8 (max 10, min 0)
+#hygiene points initial amount of 8 (max 10, min 0)
+
+#Create clean? method: return true if happiness > 7
+
+#Create happy? method: return true if happiness > 7
+
+#Create get_paid method: Accepts salary and adds to bank account.
+#return "All about the benjamins."
+
+#Create take_bath method: add 4 to hygiene.
+#return "Rub-a-dub just relaxing in teh tub"
+
+#Create work_out method: add 2 to happiness,
+#subtract 3 from hygiene,
+#return "Another one bites the dust"
+
+#Create call_friend method: accept another instance of same class
+#add 3 to happiness
+#return "Hi {friend}! It's {person}. How are you?"
+
+#Create start_conversation method: accept friend and topic
+#return conversation based on topic.
+
+end
