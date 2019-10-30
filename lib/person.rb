c0d4cacf1d3a2eb233f643fbb3c4ea26aecbe66e
github username <github email address>
2019-10-30 16:33:11 -0400

    development2

--- lib/person.rb
+++ lib/person.rb
@@ -14,22 +14,6 @@ class Person
         @hygiene_points = hygiene_points
         # binding.pry 
       end
-     
-    #   def name
-    #     @name
-    #   end
-
-    #   def name=(new_name) 
-    #     @name = new_name
-    #   end
-
-    #   def bank_account_balance
-    #     @bank_account_balance
-    #   end
-
-    #   def bank_account_balance=(new_balance)
-    #     @bank_account_balance = new_balance
-    #   end
 
       def happiness_points
         @happiness_points
@@ -45,26 +29,57 @@ class Person
       end
 
       def hygiene_points=(hygiene)
-        hygiene_value = [[hygiene.to_i , 0].max , 10].min
+        self.hygiene_value = [[hygiene.to_i , 0].max , 10].min
         @hygiene_points = hygiene_value
       end
+
+      def clean(hygiene_points)
+        if hygiene_points >7 
+          return true
+        else 
+          return false
+      end
+    end
+
+      #def happy()
+      # end
+
+      # def take_bath
+      # end
+
+      # def work_out
+      # end
+
+      # def call_friend
+      # end
+
+      # def start_conversation
+      # end
+
      
 end 
 
+
 Skyler = Person.new("Skyler" )
 
-# binding.pry 
+
+
+#final_hygiene = clean(Skyler.hygiene_points)
+
 
 Skyler.bank_account_balance = 10000000
 Skyler.hygiene_points = -10 
 Skyler.happiness_points = -10 
 Skyler.happiness_points = 100
 
-
+Skyler.name = "John"
 puts Skyler.name 
 puts Skyler.bank_account_balance 
 puts Skyler.happiness_points 
 puts Skyler.hygiene_points 
+#puts "My hygiene points are #{}"
+
+
 
 #Remi = new.Person 
 
