8cef7645f132ad11cac22522e19b55553054ee73
github username <github email address>
2020-04-07 11:24:22 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,96 +1,70 @@
-# your code goes here
 
-require 'pry'
+class Person
 
-# your code goes here
-class Person 
-    
-    attr_reader :hygiene
-    attr_accessor :bank_account
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
 
-    def initialize(name , bank_account=25, happiness=8 , hygiene=8) 
-        @name = name
-        @bank_account = bank_account
-        @happiness = happiness
-        @hygiene = hygiene
-        #binding.pry 
-      end
-     
-      def name
-        @name
-      end
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
 
-      def happiness
-        @happiness
-      end
+  def happiness=(num)
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
 
-      def happiness=(new_happy_points)
-        if new_happy_points >10 
-          @happiness = 10
-        elsif new_happy_points <= 0
-          @happiness = 0
-        else @happiness = new_happy_points 
-        end 
-      end
+  def hygiene=(num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+  end
 
-      def hygiene=(hygiene_points)
-        if hygiene_points >10 
-          @hygiene = 10
-        elsif hygiene_points <= 0
-          @hygiene = 0
-        else @hygiene = hygiene_points 
-        end 
-      end
+  def happy?
+    happiness > 7
+  end
 
-      def clean?
-        if hygiene >7 
-          return true
-        else 
-          return false
-      end
+  def clean?
+    hygiene > 7
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def start_conversation(person, topic)
+    objects = [self, person]
+    if topic == "politics"
+      objects.each { |o| o.happiness -= 2}
+      first, second = ["partisan", "lobbyist"]
+    elsif topic == "weather"
+      objects.each { |o| o.happiness += 1}
+      first, second = ["sun", "rain"]
     end
+    first ||= "blah"
+    second ||= "blah"
+    base_string = "blah blah #{first} blah #{second}"
+  end
 
-      def happy?
-        if happiness > 7
-          return true
-        else 
-          return false
-       end
-      end
+  def get_paid(salary)
+    self.bank_account += salary
+    self.happiness += 1
+    "all about the benjamins"
+  end
 
-     def get_paid(salary)
-     @bank_account = @bank_account + salary
-     return "all about the benjamins"
-     end 
+  def call_friend(friend)
+    [friend, self].each {|o| o.happiness += 3 }
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
 
-
-      def take_bath
-        self.hygiene = self.hygiene + 4 
-        return "♪ Rub-a-dub just relaxing in the tub ♫"
-      end
-
-      def work_out 
-        self.happiness = self.happiness + 2 
-        self.hygiene = self.hygiene - 3 
-        return "♪ another one bites the dust ♫"
-      end
-
-      # def call_friend
-
-      # end
-
-      def start_conversation (person , topic)
-        if topic == "politics"
-         return  "blah blah partisan blah lobbyist"
-        elsif topic = "weather"
-         return  "blah blah sun blah rain"  
-        else 
-          return "blah blah blah blah blah" 
-        end 
-      end
-
-     
-end 
-
-
-Skyler = Person.new("Skyler" )
+end
